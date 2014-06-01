1568dc8f0f89442a0df6b8e3c4531917aef6c8db
Janos G. Hajagos <risk.limits@gmail.com>
2014-06-01 13:51:17 -0400

    First commit on the Acela. Have a preliminary working code for denormalizing a table for a column that repeats based on using the SqlAlchemy library. This allows this process in theory to be applied to any database.

---
+++ normalize_table_that_has_been_denormalized.py
@@ -0,0 +1,146 @@
+__author__ = 'Janos Hajagos'
+
+# Taking a table detect fields that repeat and then generate additional tables that denormalize the table
+
+
+
+import sqlalchemy as sa
+import re
+
+def main():
+    pass
+
+
+def create_table_that_normalize_repeated_column(table_name, field_name_pattern, new_table_name, engine,
+                                                identifier_column="id", mapped_identifier_column="mapped_id",
+                                                sequence_field_name="sequence_id", schema=None):
+
+    metadata = reflect_metadata(engine, schema)
+
+    columns = get_column_names_from_table(table_name, metadata)
+    columns_dict = get_column_names_with_types_dict(table_name, metadata)
+
+    columns_that_repeat = get_columns_that_appear_to_repeat(columns, field_name_pattern)
+
+    data_type = columns_dict[columns_that_repeat[0][0]]
+
+    data_type_id_field = columns_dict[identifier_column]
+
+    columns_to_add = []
+    columns_to_add += [sa.Column(mapped_identifier_column, data_type_id_field)]
+
+    if field_name_pattern[-1] == " " or field_name_pattern[-1] == "_":
+        base_field_name = field_name_pattern[:-1]
+    else:
+        base_field_name = field_name_pattern
+
+    columns_to_add += [sa.Column(sequence_field_name, sa.Integer())]
+
+    columns_to_add += [sa.Column(base_field_name, data_type)]
+
+    new_table = sa.Table(new_table_name, metadata, *columns_to_add)
+    metadata.create_all()
+
+    return new_table
+
+
+def create_insert_for_column_that_repeats(new_table, old_table, metadata, identifier_to_map, repeated_column,
+                                          sequence_id, mapped_identifier, mapped_column_that_repeats, sequence_column_name):
+    """
+    In [34]: print(dx.insert().from_select(["mapped_id","dx","sequence_id"], sa.sql.
+select([dn.c.id, dn.c.dx1, sa.sql.literal_column("1")]).where(dn.c.dx1 != None))
+INSERT INTO diagnosis (mapped_id, dx, sequence_id) SELECT denormalized_table.id,
+ denormalized_table.dx1, 1
+FROM denormalized_table
+WHERE denormalized_table.dx1 IS NOT NULL
+    """
+
+    new_table = metadata.tables[new_table]
+    old_table = metadata.tables[old_table]
+
+    identifier_column_obj = old_table.columns[identifier_to_map]
+    column_that_repeats = old_table.columns[repeated_column]
+
+    select_sql_expr = sa.sql.select([identifier_column_obj, column_that_repeats, sa.sql.literal_column(str(sequence_id))]).where(column_that_repeats != None)
+    insert_sql_expr = new_table.insert().from_select([mapped_identifier, sequence_column_name, mapped_column_that_repeats], select_sql_expr)
+
+    return insert_sql_expr
+
+
+def normalize_columns_that_repeat(table_name, new_table_name, pattern_to_match, engine, identifier_column="id",
+                                 identifier_column_that_maps="mapped_id", sequence_field_name="sequence_id", schema=None):
+
+    """Normalize a table by looking for patterns that repeat in the table columns based on a pattern"""
+    #TODO: pattern_to_match is not robust to ["dx_" or "dx "] need to strip these characters
+    metadata = reflect_metadata(engine, schema=schema)
+    column_names = get_column_names_from_table(table_name, metadata)
+    column_names_that_repeat = get_columns_that_appear_to_repeat(column_names, search_pattern=pattern_to_match)
+    new_table_obj = create_table_that_normalize_repeated_column(table_name, pattern_to_match, new_table_name, engine,
+                                                                identifier_column, identifier_column_that_maps,
+                                                                sequence_field_name, schema)
+    metadata = reflect_metadata(engine, schema=schema)
+    sql_to_execute = []
+    for repeat_column in column_names_that_repeat:
+        sql_expr = create_insert_for_column_that_repeats(new_table_name, table_name, metadata, identifier_column,
+                                                         repeat_column[0], repeat_column[1], identifier_column_that_maps,
+                                                         pattern_to_match, sequence_field_name)
+
+        sql_to_execute += [str(sql_expr)]
+
+    connection = engine.connect()
+
+    for sql_statement in sql_to_execute:
+        print(sql_statement)
+        engine.execute(sql_statement)
+
+    return sql_to_execute
+
+
+def get_data_table(table_name, metadata):
+    return metadata.table[table_name]
+
+
+def reflect_metadata(engine, schema=None):
+    meta = sa.MetaData(engine)
+    meta.reflect()
+    return meta
+
+
+def get_column_names_from_table(table_name, metadata):
+    table = metadata.tables[table_name]
+    columns = list(table.columns)
+    column_list = [c.name for c in columns]
+    return column_list
+
+
+def get_column_names_with_types_dict(table_name, metadata):
+    table = metadata.tables[table_name]
+    columns = list(table.columns)
+    column_data_type_dict = {}
+    for column in columns:
+        column_data_type_dict[column.name] = column.type
+    return column_data_type_dict
+
+
+def get_columns_that_appear_to_repeat(list_of_fields, search_pattern):
+    """Matches columns that have a regular pattern that has a number suffix"""
+    string_to_suffix = r"([0-9]*$)"
+
+    re_search_pattern_string = search_pattern + string_to_suffix
+    re_search_pattern = re.compile(re_search_pattern_string)
+
+    list_of_fields_match = []
+    for field in list_of_fields:
+        match_result = re.match(re_search_pattern, field)
+        if match_result:
+            matched_results = match_result.groups()
+            number_matched = matched_results[0]
+
+            list_of_fields_match.append((field, int(number_matched), search_pattern))
+
+    list_of_fields_match.sort(key=lambda x: x[1])
+    return list_of_fields_match
+
+
+if __name__ == "__main__":
+    pass
---
+++ test/test_normalize_table_that_has_been_denormalized.py
@@ -0,0 +1,74 @@
+__author__ = 'janos'
+
+import sys
+sys.path.insert(0, "../")
+
+import unittest
+import sqlalchemy as sa
+import os
+
+from sqlalchemy import INTEGER
+
+import normalize_table_that_has_been_denormalized as ntd
+
+class TestGetColumnsThatRepeatFromDatabase(unittest.TestCase):
+    def setUp(self):
+        if os.path.exists("test.db3"):
+            os.remove("test.db3")
+        self.engine = sa.create_engine("sqlite:///test.db3")
+        self.connection = self.engine.connect()
+        self.connection.execute("create table denormalized_table (id integer, date_of_service date, dx1 varchar(8), dx2 varchar(8), dx3 varchar(8));")
+
+        self.connection.execute("insert into denormalized_table (id, date_of_service, dx1) values (1, '2012-01-01','250.00')")
+        self.connection.execute("insert into denormalized_table (id, date_of_service, dx1, dx2) values (5, '2012-01-01','250.00','401.00')")
+        self.connection.execute("insert into denormalized_table (id, date_of_service, dx1, dx2, dx3) values (11, '2012-01-01','250.00','250.01','401.00')")
+
+        self.metadata = ntd.reflect_metadata(self.engine)
+
+    def test_get_column_names(self):
+        column_names = ntd.get_column_names_from_table("denormalized_table", self.metadata)
+        self.assertEquals(["id", "date_of_service", "dx1", "dx2", "dx3"], column_names)
+
+    def test_get_column_dict(self):
+        column_dict = ntd.get_column_names_with_types_dict("denormalized_table", self.metadata)
+
+        self.assertTrue("id" in column_dict)
+        self.assertEquals(INTEGER().__class__, column_dict["id"].__class__)
+
+    def test_create_table_for_normalization(self):
+
+        ntd.create_table_that_normalize_repeated_column("denormalized_table", "dx", "diagnosis", self.engine,
+                                                identifier_column="id", mapped_identifier_column="mapped_id",
+                                                sequence_field_name="sequence_id", schema=None)
+
+    def test_create_table_and_insert(self):
+
+        ntd.normalize_columns_that_repeat('denormalized_table', "diagnosis", "dx", self.engine, "id", "mapped_id",
+                                          sequence_field_name="sequence_id")
+
+
+class TestGetColumnsThatAppearToRepeat(unittest.TestCase):
+    def setUp(self):
+        self.field_names = ["id", "identifier", "DX 1", "DX 2", "DX 3", "Proc_Code_2", "Proc_Code_1"]
+
+    def test_identify_columns_that_repeat(self):
+        columns_that_repeat1 = ntd.get_columns_that_appear_to_repeat(self.field_names, search_pattern="DX ")
+        self.assertTrue(len(columns_that_repeat1))
+
+        first_element1 = columns_that_repeat1[0]
+        self.assertEquals(tuple, first_element1.__class__)
+        self.assertEquals(int, first_element1[1].__class__)
+
+        self.assertEquals(1, first_element1[1])
+
+        import pprint
+        pprint.pprint(columns_that_repeat1)
+
+        columns_that_repeat2 = ntd.get_columns_that_appear_to_repeat(self.field_names, search_pattern="Proc_Code_")
+        self.assertTrue(len(columns_that_repeat2))
+
+        first_element2 = columns_that_repeat2[0]
+        self.assertEquals(tuple, first_element2.__class__)
+        self.assertEquals(int, first_element2[1].__class__)
+
+        self.assertEquals(1, first_element2[1])
