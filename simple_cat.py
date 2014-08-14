6529395fa0cfc07c57e16af789a0aa0a9cda5600
jhajagos <risk.limits@gmail.com>
2014-08-14 08:08:46 -0400

    Added a flag for including the file name

--- simple_cat.py
+++ simple_cat.py
@@ -4,15 +4,40 @@ import sys
 import glob
 
 
-def main(in_files, out_file="simple_cat_result.txt"):
+def main(in_files, has_header=True, out_file="simple_cat_result.txt", add_file_name=1):
     with open(out_file, "w") as fw:
+        i = 0
         for in_file in in_files:
             with open(in_file, "r") as f:
+                j = 0
                 for line in f:
-                    fw.write(line)
+                    if has_header:
+                        if i == 0 or (i > 0 and j > 0):
+
+                            if i == 0 and j == 0:
+                                if add_file_name:
+                                    print("hi")
+                                    line = line.lstrip()[:-1] + "," + "file_name" + "\n"
+
+                            else:
+                                if add_file_name:
+                                    line = line.lstrip()[:-1] + "," + in_file + "\n"
+                            fw.write(line)
+                            j += 1
+                        else:
+
+                            j += 1
+                    else:
+                        fw.write(line)
+                        j += 1
+                print("Read %s lines in '%s'" % (j, in_file))
+                i += 1
 
 if __name__ == "__main__":
     if "*" in sys.argv[1]:
-        main(glob.glob(sys.argv[1]))
+        if len(sys.argv) == 1:
+            main(glob.glob(sys.argv[1]))
+        else:
+            main(glob.glob(sys.argv[1]), has_header=bool(sys.argv[2]), out_file=sys.argv[3])
     else:
         main(sys.argv[1:])
